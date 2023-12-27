ebf57ae8256540c3292816abc5643d27e88c3971
Ethan-Selvarajah2 <ethan.selvarajah25@ycdsbk12.ca>
2023-12-27 11:45:28 -0500

    Basic Movement Controls

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,20 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
+	boolean boolUpPressed;
+  boolean boolLeftPressed;
+  boolean boolDownPressed;
+  boolean boolRightPressed;
+	float fltPlayerX;
+  float fltPlayerY;
+  float fltPlayerSpeed;
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+	// Size call
+    size(1280, 720);
   }
 
   /** 
@@ -17,20 +23,79 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
+    boolUpPressed = false;
+    boolLeftPressed = false;
+    boolDownPressed = false;
+    boolRightPressed = false;
+    fltPlayerX = 640;
+    fltPlayerY = 360;
+    fltPlayerSpeed = 1;
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    background(210, 255, 173);
+	  ellipse(fltPlayerX, fltPlayerY, 100, 100);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Vertical and horizontal movement
+    if (boolUpPressed) {
+      fltPlayerY -= fltPlayerSpeed;
+    } else if (boolLeftPressed) {
+      fltPlayerX -= fltPlayerSpeed;
+    } else if (boolDownPressed) {
+      fltPlayerY += fltPlayerSpeed;
+    } else if (boolRightPressed) {
+      fltPlayerX += fltPlayerSpeed;
+    }
+
+    // Diagonal movement
+    if (boolUpPressed && boolLeftPressed) {
+      fltPlayerY -= fltPlayerSpeed;
+      fltPlayerX -= fltPlayerSpeed;
+    } else if (boolUpPressed && boolRightPressed) {
+      fltPlayerY -= fltPlayerSpeed;
+      fltPlayerX += fltPlayerSpeed;
+    } else if (boolDownPressed && boolLeftPressed) {
+      fltPlayerY += fltPlayerSpeed;
+      fltPlayerX -= fltPlayerSpeed;
+    } else if (boolDownPressed && boolRightPressed) {
+      fltPlayerY += fltPlayerSpeed;
+      fltPlayerX += fltPlayerSpeed;
+    }
+  }
+
+  /**
+   * Checks if any arrow keys are pressed
+   * Changes the boolean flag to true based on the keyCode pressed
+   */
+  public void keyPressed() {
+    if (keyCode == UP) {
+      boolUpPressed = true;
+    } else if (keyCode == LEFT) {
+      boolLeftPressed = true;
+    } else if (keyCode == DOWN) {
+      boolDownPressed = true;
+    } else if (keyCode == RIGHT) {
+      boolRightPressed = true;
+    }
+  }
+
+  /**
+   * Checks if any arrow keys are released
+   * Changes the boolean flag to false based on the keyCode pressed
+   */
+  public void keyReleased() {
+    if (keyCode == UP) {
+      boolUpPressed = false;
+    } else if (keyCode == LEFT) {
+      boolLeftPressed = false;
+    } else if (keyCode == DOWN) {
+      boolDownPressed = false;
+    } else if (keyCode == RIGHT) {
+      boolRightPressed = false;
+    }
   }
   
-  // define other methods down here.
 }
