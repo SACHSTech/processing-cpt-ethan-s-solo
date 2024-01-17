85bafc703eb81f87f8e789acc615fd0e98529db7
Ethan-Selvarajah2 <ethan.selvarajah25@ycdsbk12.ca>
2024-01-16 21:09:52 -0500

    Basic Space Invaders Code (Sketch 2) - (Ignore other sketches)

--- Sketch2.java
+++ Sketch2.java
@@ -1,14 +1,22 @@
 import processing.core.PApplet;
+import java.util.ArrayList;
 
 public class Sketch2 extends PApplet {
+	int intPlayerX;
+  int intPlayerY;
+  int[] intEnemyX;
+  int[] intEnemyY;
+  int intEnemyMaxCount = 10;
 	
-	
+  ArrayList<Float> fltProjectileX = new ArrayList<>();
+  ArrayList<Float> fltProjectileY = new ArrayList<>();
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(720, 720);
   }
 
   /** 
@@ -17,20 +25,55 @@ public class Sketch2 extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
+    intPlayerX = width / 2;
+    intPlayerY = height - 45;
+
+    intEnemyX = new int[intEnemyMaxCount];
+    intEnemyY = new int[intEnemyMaxCount];
+
+    for (int i = 0; i < intEnemyMaxCount; i++) {
+      intEnemyX[i] = (int) random(width);
+      intEnemyY[i] = (int) random(-height, -20);
+    }
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	  background(0, 0, 255);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    fill(255, 0, 0);
+    ellipse(intPlayerX, intPlayerY, 20, 20);
+
+    fill(0, 255, 0);
+    for (int i = 0; i < intEnemyMaxCount; i++) {
+      ellipse(intEnemyX[i], intEnemyY[i], 20, 20);
+      intEnemyY[i] += 2;
+      
+      if (intEnemyY[i] > height) {
+        intEnemyX[i] = (int) random(width);
+        intEnemyY[i] = (int) random(-height, -20);
+      }
+    }
+    
+    for (int i = 0; i < intEnemyMaxCount; i++) {
+      if (dist(intPlayerX, intPlayerY, intEnemyX[i], intEnemyY[i]) < 20 / 2 + 20 / 2) {
+        background(0, 0, 255);
+        fill(0);
+        textAlign(CENTER);
+        textSize(30);
+        text("Game Over", width / 2, height / 2);
+        noLoop();
+      }
+    }
   }
   
-  // define other methods down here.
+  public void keyPressed() {
+    if (keyCode == LEFT && intPlayerX > 0) {
+      intPlayerX -= 10;
+    } else if (keyCode == RIGHT && intPlayerX < width) {
+      intPlayerX += 10;
+    }
+  }
 }
